7a1ae9c09cc8d204310a5ce7178c3db824a29a16
M.J. Azimov <azimovm.j@gmail.com>
2022-05-25 21:41:05 -0400

    Adding if Statement to the in keyword

--- 09_the_in_keyword/code.py
+++ 09_the_in_keyword/code.py
@@ -4,3 +4,10 @@ print("Jen" in friends)
 #This method is giving true or false boolean result
 movies_watched = {"The Matrix", "Green Bool", "Her"}
 user_movie = input("Enter Somethins you've watched recently: ")
+
+if user_movie in movies_watched:
+    print(f"I've watched { user_movie} too!")
+else:
+    print("I haven't watched  that yet.")
+
+
