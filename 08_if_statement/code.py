5a31dc8a8036ac743ea5fc961073219fc44fa6b7
M.J. Azimov <azimovm.j@gmail.com>
2022-05-25 21:21:11 -0400

    if_statement adding all other days of the week

--- 08_if_statement/code.py
+++ 08_if_statement/code.py
@@ -2,6 +2,18 @@ day_of_week = input("What day of the week is today?").lower()
 
 if day_of_week == "monday":
     print("Have great Start to your week")
+elif day_of_week == "tuesday":
+    print("it's Tuesday")
+elif day_of_week == "wednesday":
+    print("It's Wednesday")
+elif day_of_week == "thursday":
+    print("It's Thursday")
+elif day_of_week == "friday":
+    print("It's Friday")
+elif day_of_week == "saturday":
+    print("It's Saturday")
+elif day_of_week == "sunday":
+    print("It's Sunday")
 else:
     print("Full speed ahead")
 
