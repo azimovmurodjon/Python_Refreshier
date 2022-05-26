d2572579ce2a682502fa08dffc46405ec3677245
M.J. Azimov <azimovm.j@gmail.com>
2022-05-25 21:02:43 -0400

    boolean class is updated

---
+++ 07_booleans/code.py
@@ -0,0 +1,16 @@
+#we get True
+print(5 == 5)
+#we get False
+print (5 > 5)
+# We Should get false
+print(10 != 10)
+
+#Comparisons: ==, !=, >, <, >=, <=
+
+friends = ["Tom","Bob"]
+adboad = ["Tom","Bob"]
+
+#It returns True
+print(friends == adboad)
+#It returns False
+print(friends is adboad)
