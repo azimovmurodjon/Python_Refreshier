7e2c825ed88449ce64b4d228a67a67b72bdf96ca
M.J. Azimov <azimovm.j@gmail.com>
2022-05-25 21:31:49 -0400

    the_in_keyword last comments added. ready to be merged

--- 09_the_in_keyword/code.py
+++ 09_the_in_keyword/code.py
@@ -1,6 +1,7 @@
 friends = ["Bob", "Jen", "Tom"]
 print("Jen" in friends)
 
+#This method is giving true or false boolean result
 movies_watched = {"The Matrix", "Green Bool", "Her"}
 user_movie = input("Enter Somethins you've watched recently: ")
 print(user_movie in movies_watched)
