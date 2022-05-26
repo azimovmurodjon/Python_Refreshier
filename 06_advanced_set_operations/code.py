69b02e298b668c155166a77e818af0891b37b96c
M.J. Azimov <azimovm.j@gmail.com>
2022-05-25 20:19:12 -0400

    advanced_set_operations is updated

--- 06_advanced_set_operations/code.py
+++ 06_advanced_set_operations/code.py
@@ -8,3 +8,17 @@ print(local_Friends)
 #This method will return empty set()
 local_Friends = abroad.difference(friends)
 print(local_Friends)
+
+#This method will United all friends together
+local = {"Rolf"}
+abroad = {"Bob", "Anne"}
+
+friends = local.union(abroad)
+print(friends)
+
+#This method will print Duplicate one
+art = {"Bob","Jen","Rolf","Charlie"}
+scince = {"Bob","Jen","Adam","Anne"}
+
+both  = art.intersection(scince)
+print(both)
