6102fa7580858e87a7f97ff95d8e6795465156ae
M.J. Azimov <azimovm.j@gmail.com>
2022-05-25 22:03:13 -0400

    if_wit_in is updated

--- 10_if_with_in/code.py
+++ 10_if_with_in/code.py
@@ -1,4 +1,28 @@
+ #Majic Number App
+# #Will be able to enter Upper and lowwer case for Y diffrent method
+# number = 7
+# user_input = input("Enter 'y' if you would like to play: ").lower()
+#
+# if user_input == ("y"):
+#     user_input = int(input("Guess our Number: "))
+#     if user_input == number:
+#         print("You guessed correcty!")
+#     else:
+#         print("Sorry, it's wrong!")
+#
+# #Will be able to enter Upper and lowwer case for Y diffrent method
+# number = 15
+# user_input = input("Enter 'y' if you would like to play: ")
+#
+# if user_input in ("y", "Y"):
+#     user_input = int(input("Guess our Number: "))
+#     if user_input == number:
+#         print("You guessed correcty!")
+#     else:
+#         print("Sorry, it's wrong!")
 
+#Will be able to enter Upper and lowwer case for Y diffrent method and gues was close or not will mentioned
+number = 7
 user_input = input("Enter 'y' if you would like to play: ").lower()
 
 if user_input == ("y"):
