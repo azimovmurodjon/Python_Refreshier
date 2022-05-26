d9208c4fdd044bcabd7d991e6c83d23b851bf54c
M.J. Azimov <azimovm.j@gmail.com>
2022-05-25 21:44:44 -0400

    Adding majic number app to the in keyword

--- 09_the_in_keyword/code.py
+++ 09_the_in_keyword/code.py
@@ -10,4 +10,14 @@ if user_movie in movies_watched:
 else:
     print("I haven't watched  that yet.")
 
+#Majic Number App
 
+number = 7
+user_input = input("Enter 'y' if you would like to play: ")
+
+if user_input== "y":
+    user_input = int(input("Guess our Number: "))
+    if user_input == number:
+        print("You guessed correcty!")
+    else:
+        print("Sorry, it's wrong!")
