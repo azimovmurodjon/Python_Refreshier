c297a99189e37c148a8368e3a2415c1c34b6e50b
M.J. Azimov <azimovm.j@gmail.com>
2022-05-25 21:28:37 -0400

    the_in_keyword Watched movie method is created

--- 09_the_in_keyword/code.py
+++ 09_the_in_keyword/code.py
@@ -1,2 +1,6 @@
 friends = ["Bob", "Jen", "Tom"]
 print("Jen" in friends)
+
+movies_watched = {"The Matrix", "Green Bool", "Her"}
+user_movie = input("Enter Somethins you've watched recently: ")
+print(user_movie in movies_watched)
